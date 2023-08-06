41d436b7cd855bb911e9b6f16695c085d9425cce
H8K56 <khadrahussein4@gmail.com>
2023-08-06 18:00:44 +0000

    Added a check video function,now runs video without

--- Yolov8/tempKeypoint.py
+++ Yolov8/tempKeypoint.py
@@ -3,45 +3,7 @@ from tqdm import tqdm
 from ultralytics import YOLO
 from utils import count_lines
 from keypoints_analisis import generate_graph, direction_pitch, direction_yaw, save_pitch_yaw_headsize_origin
-
-
-# Parse keypoints from CSV file
-def parse_keypoints_csv(file_path):
-    try:
-        print(file_path)
-        with open(file_path, 'r') as file:
-            l = count_lines(file_path)
-            file.seek(0)  # Reset file pointer
-
-            reader = csv.reader(file)
-            next(reader)  # Skip header row
-
-            keypoints = []
-            for i, row in tqdm(enumerate(reader), total=l, desc="Parse progress:"):
-                if row:
-                    frame, person, keypoint, x, y = row
-                    keypoints.append((int(frame), int(person), int(keypoint), float(x), float(y)))
-            return keypoints
-    except Exception as e:
-        print(f"Error parsing keypoints from CSV file: {e}")              
-
-
-# Save keypoints to CSV file
-def save_keypoints(results, output_file):
-    with open(output_file, 'a', newline='') as file:
-        if count_lines(output_file) > 2:
-            raise Exception("The file already has the necessary.")
-        writer = csv.writer(file)
-
-        l = len(results)
-        for i in tqdm(range(l), desc="Parse progress:"):
-            result = results[i]
-            keypoints = result.keypoints
-            for j, person_keypoints in enumerate(keypoints):
-                for k, keypoint in enumerate(person_keypoints):
-                    x, y = keypoint[:2]
-                    writer.writerow([i, j, k, x.item(), y.item()])
-
+import os
 
 # Separate the keypoints by frame and person
 def separate_keypoints(keypoints):
@@ -68,27 +30,32 @@ def display_keypoints(keypoints, save_graph=False):
             print()
         print()
 
+def check_video_file(source):
+    if not os.path.isfile(source):
+        print(f"File '{source}' does not exist.")
+        return False
+    
+    source_extension = os.path.splitext(source)[1].lower()
+    valid_extensions = [".mp4", ".mov", ".mpeg"]
 
-model = YOLO("yolov8n-pose.pt")
+    if source_extension not in valid_extensions:
+        print("File type is incorrect.")
+        return False
 
-source = "bazzar.mp4"
-output_file = "./keypoints_output.csv"
+    return True
 
+def main():
+    model = YOLO("yolov8n-pose.pt")
 
-# Save or parse the keypoints to a CSV filex
-if input("\nSave or parse the keypoints? (1/2): ") == "1":
-    results = model.predict(source=source, save=False, conf=0.5, save_txt=False, show=True)
-    save_keypoints(results, output_file)
-else:
-    parsed_results = parse_keypoints_csv(output_file)
-
-if input("\nDisplay the keypoints? (y/n): ") == "y" and parsed_results:
-    separated_keypoints = separate_keypoints(parsed_results)
-    display_keypoints(separated_keypoints, save_graph=True)
-
-if input("\nSave rotation keypoints? (y/n): ") == "y" and parsed_results:
-    separated_keypoints = separate_keypoints(parsed_results)
-    save_pitch_yaw_headsize_origin(separated_keypoints, 'pitch_yaw_headsize.json')
+    video_file = "bazzar.mp4"
 
 
-print("Done!")
+    if check_video_file(video_file):
+        results = model.predict(source=video_file, save=False, conf=0.5, save_txt=False, show=True)
+        separated_keypoints = separate_keypoints(results)
+        save_pitch_yaw_headsize_origin(separated_keypoints, 'pitch_yaw_headsize.json')
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
